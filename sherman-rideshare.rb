277c4c0741932672342bf9ef94a72cb0bf2409ad
Xtina <christinasherman206@gmail.com>
2018-08-15 21:54:34 -0700

    Update sherman-rideshare.rb

--- sherman-rideshare.rb
+++ sherman-rideshare.rb
@@ -1,10 +1,9 @@
-# The project is not fully completed, but its what i'm happy with so far. I'll keep at it for the next couple days.
-
+# This project is doing me a major frustrate. Despite much effort I cannot get the driver rating to iterate properly.
 all_drivers = {"DR0001" =>
   [{"date" => "2016-02-03",
     :cost => 10,
     "rider_ID" => "RD0003",
-    "rating" => 3 
+    "rating" => 3
   },
   {
     "date" => "2016-02-03",
@@ -39,43 +38,50 @@ all_drivers = {"DR0001" =>
 }
 ],
 "DR0003" =>
- [{:date => "2016-02-04",
-   :cost => 5,
-   :rider_ID => "RD0066",
-   :rating => 5
- },
- {
-   :date => "2016-02-05",
-   :cost => 50,
-   :rider_ID => "RD0003",
-   :rating => 2
- }
+[{:date => "2016-02-04",
+  :cost => 5,
+  :rider_ID => "RD0066",
+  :rating => 5
+},
+{
+  :date => "2016-02-05",
+  :cost => 50,
+  :rider_ID => "RD0003",
+  :rating => 2
+}
 ],
 "DR0004" =>
-  [{
-    :date => "2016-02-03",
-    :cost => 5,
-    :rider_ID => "RD0022",
-    :rating => 5
-  },
-  {
-    :date => "2016-02-04",
-    :cost => 10,
-    :rider_ID => "RD0022",
-    :rating => 4}
-]}
+[{
+  :date => "2016-02-03",
+  :cost => 5,
+  :rider_ID => "RD0022",
+  :rating => 5
+},
+{
+  :date => "2016-02-04",
+  :cost => 10,
+  :rider_ID => "RD0022",
+  :rating => 4}
+  ]}
 
-all_drivers.each do |key, value|
-  date_rides = value.sort_by {|array| array[:date]}
-  cost_total = date_rides.map do |array| array[:cost]
+  all_drivers.each do |key, value|
+    date_rides = value.sort_by {|array| array[:date]}
+    cost_total = date_rides.map do |array| array[:cost]
+    end
+    puts "#{key} has earned $#{cost_total.sum}."
+    puts "#{key} has given #{cost_total.length} rides.\n\n"
   end
-  puts "#{key} has earned $#{cost_total.sum}."
-  puts "#{key} has given #{cost_total.length} rides.\n\n"
+
+  all_drivers.each do |key, value|
+    rider_rating = value.sort_by {|array| array[:rider_ID]}
+    driver_rating = rider_rating.map do |array| array[:rating]
+    end
+    puts driver_rating
+    puts "#{key} has a rating of #{driver_rating.length}"
   end
- 
-#   all_drivers.each do |key, value|
-#   ratings = all_drivers.sort_by {|value| [:rating]}
-#   driver_rating = ratings.map do |value| [:rating]
-#   end
-#   puts "#{key} has an average rating of #{driver_rating.sum/driver_rating.length}."
-# end
+
+  # *******************
+  # reduce(:+)/driver_rating.length.to_f
+  # driver_rating.reduce(:+)/driver_rating.length.to_f
+  # puts driver_rating.reduce(:+).to_f / driver_rating.size
+  #  single_rating = driver_rating.reduce(:+)/driver_rating.length.to_f
